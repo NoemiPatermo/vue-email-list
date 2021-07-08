85c899914b57e2c45cddf3898b6d0043f4bb1c4a
noemi86-cpu <noemipatermo@gmail.com>
2021-07-08 17:43:35 +0200

    email-list

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 Vue.config.devtools = true;
+
 new Vue ({
     el:"#app",
     data: {
-        
-    }
-})
+        emailList:[],
+    },// chiude il data
+    created () {
+        this.getEmail();
+    },
+    
+   methods:{
+       getEmail: function(){
+
+           for(let i=0; i < 10, i++;){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                this.emailList.push(response.data.response);
+                console.log(this.emailList)
+                })
+            }//chiude il for
+           
+       }//chiude la function
+   },
+});//chiude la vue
